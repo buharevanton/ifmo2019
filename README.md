e26a5ff573336f031eecec055b5a607ba5091cfc
buharevanton <buharev.anton@gmail.com>
2020-03-03 17:54:06 +0300

    bot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ifmo2019
-ifmo-2019
+Bot azure botframework
+
+![image](https://user-images.githubusercontent.com/47527927/75787329-9aedf980-5d77-11ea-8931-fe6749811cd9.png)
+
+![image](https://user-images.githubusercontent.com/47527927/75787406-bb1db880-5d77-11ea-8ee8-d982bb218857.png)
+
+![image](https://user-images.githubusercontent.com/47527927/75787524-e56f7600-5d77-11ea-883e-2448f5427dab.png)
---
+++ source/bot.js
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler } = require('botbuilder');
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        
+        console.log('sgfsdfg')
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+         this.onMessage(async (context, next) => {
+            let result = `Вы сказали:  '${ context.activity.text }'`;
+            if (context.activity.text === 'Привет') {
+                result = 'Тоже приветствую';
+            }
+            await context.sendActivity(result);
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity('Hello and welcome!');
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.EchoBot = EchoBot;
